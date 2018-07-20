52febe005d610bf9b614b3d7efa0cfe5e0318f40
吴鑫康 <zjxjwxk@gmail.com>
2018-07-20 14:01:18 +0800

    Test main

--- 203_Remove_LinkedList_Elements/src/Solution.java
+++ 203_Remove_LinkedList_Elements/src/Solution.java
@@ -25,4 +25,13 @@ class Solution {
 
         return head;
     }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        (new Solution()).removeElements(head, 6);
+        System.out.println(head);
+    }
 }
--- 203_Remove_LinkedList_Elements/src/Solution2.java
+++ 203_Remove_LinkedList_Elements/src/Solution2.java
@@ -18,4 +18,13 @@ class Solution2 {
 
         return dummyHead.next;
     }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        (new Solution2()).removeElements(head, 6);
+        System.out.println(head);
+    }
 }
