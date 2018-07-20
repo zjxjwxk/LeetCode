e46a49c399dc16b5afac83d3c712ba015fd5f074
吴鑫康 <zjxjwxk@gmail.com>
2018-07-20 18:21:19 +0800

    Recursion implement

---
+++ 203_Remove_LinkedList_Elements/src/Sum.java
@@ -0,0 +1,20 @@
+public class Sum {
+
+    public static int sum(int[] arr) {
+        return sum(arr, 0);
+    }
+
+    // 计算arr[l...n)这个区间内所有数字的和
+    private static int sum(int[] arr, int l) {
+        if (l == arr.length) {
+            return 0;
+        } else {
+            return arr[l] + sum(arr, l + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] sums = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println(sum(sums));
+    }
+}
