51ceaac314b8c4e387d667ad7b6d9551cf7d16e6
zjxjwxk <zjxjwxk@gmail.com>
2018-08-21 22:51:31 +0800

    1_Two_Sum

--- 1_Two_Sum/src/Solution.java
+++ 1_Two_Sum/src/Solution.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Main {
+public class Solution {
 
     public static void main(String[] args) {
         int nums[] = {2, 7, 11, 15};
