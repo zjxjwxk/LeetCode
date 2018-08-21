fdb72c97d4d9179b3c2d06a81a66609fdd79b9a4
zjxjwxk <zjxjwxk@gmail.com>
2018-08-21 22:04:00 +0800

    1_Two_Sum

---
+++ 1_Two_Sum/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int nums[] = {2, 7, 11, 15};
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
