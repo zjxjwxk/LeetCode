45ffe6a46ef1385d719021c018099f2af7625d27
zjxjwxk <zjxjwxk@gmail.com>
2018-12-29 11:25:07 +0800

    035

---
+++ 035-Search-Insert-Position/src/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * 直接遍历搜索
+ * @author zjxjwxk
+ */
+public class Solution {
+
+    public int searchInsert(int[] nums, int target) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (target <= nums[i]) {
+                return i;
+            }
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {1, 3, 5, 6};
+        Solution solution = new Solution();
+        System.out.println(solution.searchInsert(nums, 0));
+    }
+}
