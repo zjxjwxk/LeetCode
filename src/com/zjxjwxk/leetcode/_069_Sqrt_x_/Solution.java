2d0c334ec6f530a9f6f88c2405201b625aa38260
zjxjwxk <zjxjwxk@gmail.com>
2018-12-31 21:15:37 +0800

    069 Newton's method

---
+++ src/com/zjxjwxk/leetcode/_069_Sqrt_x_/Solution.java
@@ -0,0 +1,21 @@
+package com.zjxjwxk.leetcode._069_Sqrt_x_;
+
+/**
+ * 牛顿迭代法
+ * @author zjxjwxk
+ */
+public class Solution {
+
+    public int mySqrt(int x) {
+        long n = x;
+        while (n * n > x) {
+            n = (n + x / n) / 2;
+        }
+        return (int) n;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.mySqrt(27));
+    }
+}
