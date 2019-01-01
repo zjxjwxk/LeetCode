a352964f7cefebdbf7659db2b90fcfa9ec3adc9b
zjxjwxk <zjxjwxk@gmail.com>
2019-01-01 22:52:03 +0800

    070 iteration

---
+++ src/com/zjxjwxk/leetcode/_070_Climbing_Stairs/Solution2.java
@@ -0,0 +1,25 @@
+package com.zjxjwxk.leetcode._070_Climbing_Stairs;
+
+/**
+ * 直接迭代
+ * 并用仅两个变量减少空间复杂度
+ * @author zjxjwxk
+ */
+public class Solution2 {
+
+    public int climbStairs(int n) {
+        int a = 1;
+        int b = 1;
+        while (--n > 0) {
+            b += a;
+            a = b - a;
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        Solution2 solution2 = new Solution2();
+        System.out.println(solution2.climbStairs(44));
+    }
+}
+
