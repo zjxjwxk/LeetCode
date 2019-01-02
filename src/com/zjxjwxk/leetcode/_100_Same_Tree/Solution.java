4d2b465993e93c8895f55dc2cbafa67340d90a5b
zjxjwxk <zjxjwxk@gmail.com>
2019-01-02 15:45:18 +0800

    100

---
+++ src/com/zjxjwxk/leetcode/_100_Same_Tree/Solution.java
@@ -0,0 +1,29 @@
+package com.zjxjwxk.leetcode._100_Same_Tree;
+
+/**
+ * 递归（前序遍历）
+ * @author zjxjwxk
+ */
+public class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else{
+            return false;
+        }
+    }
+}
