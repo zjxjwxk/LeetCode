3c77b5f32cd7ac29b26257bc79294fa48c0263c0
zjxjwxk <zjxjwxk@gmail.com>
2019-01-02 15:52:44 +0800

    101 recursive

---
+++ src/com/zjxjwxk/leetcode/_101_Symmetric_Tree/Solution.java
@@ -0,0 +1,28 @@
+package com.zjxjwxk.leetcode._101_Symmetric_Tree;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+/**
+ * 广度优先遍历（递归）
+ * @author zjxjwxk
+ */
+public class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || recursive(root.left, root.right);
+    }
+    private boolean recursive(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && recursive(p.left, q.right) && recursive(p.right, q.left);
+    }
+}
