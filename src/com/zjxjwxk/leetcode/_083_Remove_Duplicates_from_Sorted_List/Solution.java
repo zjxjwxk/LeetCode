1dd9961e581fd8fece4ada98200a35cf2c84b6f8
zjxjwxk <zjxjwxk@gmail.com>
2019-01-02 15:52:16 +0800

    083

--- src/com/zjxjwxk/leetcode/_083_Remove_Duplicates_from_Sorted_List/Solution.java
+++ src/com/zjxjwxk/leetcode/_083_Remove_Duplicates_from_Sorted_List/Solution.java
@@ -1,16 +1,16 @@
 package com.zjxjwxk.leetcode._083_Remove_Duplicates_from_Sorted_List;
 
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
 /**
  * 直接遍历
  * @author zjxjwxk
  */
 public class Solution {
-
-    public class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) { val = x; }
-    }
 
     public ListNode deleteDuplicates(ListNode head) {
         ListNode node = head;
