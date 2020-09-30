8773c34fec56513a45cb34bce1283a455dd75a8b
zjxjwxk <zjxjwxk@gmail.com>
2020-09-30 09:15:00 +0800

    add 701

---
+++ src/com/zjxjwxk/leetcode/_0701_Insert_into_a_Binary_Search_Tree/Solution.java
@@ -0,0 +1,34 @@
+package com.zjxjwxk.leetcode._0701_Insert_into_a_Binary_Search_Tree;
+
+/**
+ * @author zjxjwxk
+ * @date 2020/9/30 9:09 上午
+ */
+public class Solution {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
