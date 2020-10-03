33d4d37cdc72203a7a2728a968e486bc62fd92ec
zjxjwxk <zjxjwxk@gmail.com>
2020-10-03 10:06:26 +0800

    improve 1

--- src/com/zjxjwxk/leetcode/_0001_Two_Sum/Solution.java
+++ src/com/zjxjwxk/leetcode/_0001_Two_Sum/Solution.java
@@ -9,22 +9,20 @@ import java.util.Map;
  */
 public class Solution {
 
-    public static void main(String[] args) {
-        int nums[] = {2, 7, 11, 15};
-        System.out.println(Arrays.toString(twoSum(nums, 9)));
-    }
-
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
             map.put(nums[i], i);
         }
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement) && map.get(complement) != i) {
-                return new int[]{i, map.get(complement)};
-            }
-        }
-        throw new IllegalArgumentException("No two sum solution");
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
     }
 }
