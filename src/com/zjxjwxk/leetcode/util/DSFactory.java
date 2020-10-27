78689405a41aa66c33ab8bd47f91cb679efe48c6
zjxjwxk <zjxjwxk@gmail.com>
2020-10-27 12:35:37 +0800

    Add TreeNode

--- src/com/zjxjwxk/leetcode/util/DSFactory.java
+++ src/com/zjxjwxk/leetcode/util/DSFactory.java
@@ -1,5 +1,8 @@
 package com.zjxjwxk.leetcode.util;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @author zjxjwxk
  * @date 2020/10/13 9:11 上午
@@ -13,5 +16,29 @@ public class DSFactory {
             temp = temp.next;
         }
         return dummyHead.next;
+    }
+
+    public static TreeNode newTree(Integer[] arr) {
+        if (arr.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(arr[0]);
+        int index = 1, len = arr.length;
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty() && index < len) {
+            TreeNode node = queue.poll();
+            if (arr[index] != null) {
+                node.left = new TreeNode(arr[index]);
+                queue.offer(node.left);
+            }
+            ++index;
+            if (index < len && arr[index] != null) {
+                node.right = new TreeNode(arr[index]);
+                queue.offer(node.right);
+            }
+            ++index;
+        }
+        return root;
     }
 }
--- src/com/zjxjwxk/leetcode/util/TreeNode.java
+++ src/com/zjxjwxk/leetcode/util/TreeNode.java
@@ -1,5 +1,7 @@
 package com.zjxjwxk.leetcode.util;
 
+import java.util.Objects;
+
 /**
  * @author Xinkang Wu
  * @date 2020/10/27 10:38 上午
@@ -15,5 +17,31 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        return isEqual(this, (TreeNode) o);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, left, right);
+    }
+
+    private boolean isEqual(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        return root1.val == root2.val && isEqual(root1.left, root2.left) && isEqual(root1.right, root2.right);
     }
 }
