a1217bbe9d9369f0d937352384bebcec423e2ad1
zjxjwxk <zjxjwxk@gmail.com>
2020-12-23 22:46:39 +0800

    Add 387

--- src/com/zjxjwxk/leetcode/_0387_First_Unique_Character_in_a_String/Solution.java
+++ src/com/zjxjwxk/leetcode/_0387_First_Unique_Character_in_a_String/Solution.java
@@ -1,23 +1,20 @@
 package com.zjxjwxk.leetcode._0387_First_Unique_Character_in_a_String;
 
 /**
+ * 哈希表
  * @author zjxjwxk
  */
 public class Solution {
     public int firstUniqChar(String s) {
-
         int[] freq = new int[26];
-
         for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a'] ++;
+            freq[s.charAt(i) - 'a']++;
         }
-
         for (int i = 0; i < s.length(); i++) {
             if (freq[s.charAt(i) - 'a'] == 1) {
                 return i;
             }
         }
-
         return -1;
     }
 }
