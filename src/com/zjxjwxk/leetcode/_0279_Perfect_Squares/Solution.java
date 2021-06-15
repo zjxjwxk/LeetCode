d813eea90c462f6a55ed4d2e3c9906f3001fbba6
zjxjwxk <zjxjwxk@gmail.com>
2021-06-15 09:38:37 +0800

    Add 279

--- src/com/zjxjwxk/leetcode/_0279_Perfect_Squares/Solution.java
+++ src/com/zjxjwxk/leetcode/_0279_Perfect_Squares/Solution.java
@@ -11,9 +11,8 @@ public class Solution {
         int maxSquare = (int) Math.sqrt(n);
         int[] dp = new int[n + 1];
         for (int i = 1; i <= maxSquare; ++i) {
-            int square = i * i, min = Integer.MAX_VALUE;
+            int square = i * i;
             for (int j = square; j <= n; ++j) {
-                min = Math.min(min, dp[j - square]);
                 if (dp[j] != 0) {
                     dp[j] = Math.min(dp[j], dp[j - square] + 1);
                 } else {
