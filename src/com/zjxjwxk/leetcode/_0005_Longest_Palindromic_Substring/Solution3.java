6158a3bd24a5ac9718ae31b3c70b883702f94b7b
zjxjwxk <zjxjwxk@gmail.com>
2021-09-07 15:26:37 +0800

    Add 5 Solution3

--- src/com/zjxjwxk/leetcode/_0005_Longest_Palindromic_Substring/Solution3.java
+++ src/com/zjxjwxk/leetcode/_0005_Longest_Palindromic_Substring/Solution3.java
@@ -2,24 +2,25 @@ package com.zjxjwxk.leetcode._0005_Longest_Palindromic_Substring;
 
 /**
  * 动态规划
- * 长度从小到大遍历
+ * 从后往前遍历
  * @author Xinkang Wu
  * @date 2021/3/8 11:14
  */
-public class Solution {
+public class Solution3 {
 
     public String longestPalindrome(String s) {
         int n = s.length(), begin = 0, maxLen = 1;
         boolean[][] dp = new boolean[n][n];
-        for (int len = 1; len <= n; ++len) {
-            for (int i = 0; i + len - 1 < n; ++i) {
-                int j = i + len - 1;
-                if (len == 1) {
-                    dp[i][j] = true;
-                } else if (len == 2) {
-                    dp[i][j] = (s.charAt(i) == s.charAt(j));
-                } else {
-                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i][i] = true;
+            for (int j = i + 1; j < n; ++j) {
+                int len = j - i + 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (len == 2) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
                 }
                 if (dp[i][j] && len > maxLen) {
                     begin = i;
---
+++ src/com/zjxjwxk/leetcode/_0005_Longest_Palindromic_Substring/Solution3Test.java
@@ -0,0 +1,38 @@
+package com.zjxjwxk.leetcode._0005_Longest_Palindromic_Substring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Solution3Test {
+
+    private final Solution3 solution3 = new Solution3();
+
+    @Test
+    void longestPalindrome1() {
+        String s = "babad";
+        String ans = "aba";
+        assertEquals(ans, solution3.longestPalindrome(s));
+    }
+
+    @Test
+    void longestPalindrome2() {
+        String s = "cbbd";
+        String ans = "bb";
+        assertEquals(ans, solution3.longestPalindrome(s));
+    }
+
+    @Test
+    void longestPalindrome3() {
+        String s = "a";
+        String ans = "a";
+        assertEquals(ans, solution3.longestPalindrome(s));
+    }
+
+    @Test
+    void longestPalindrome4() {
+        String s = "ac";
+        String ans = "a";
+        assertEquals(ans, solution3.longestPalindrome(s));
+    }
+}
