bb93782a504ca438f4daee36710cfca2eebb8908
Xinkang Wu <t-xinkangwu@microsoft.com>
2022-07-18 21:04:32 +0800

    Update 104 Solution2

--- src/com/zjxjwxk/leetcode/_0104_Maximum_Depth_of_Binary_Tree/Solution2.java
+++ src/com/zjxjwxk/leetcode/_0104_Maximum_Depth_of_Binary_Tree/Solution2.java
@@ -1,7 +1,5 @@
 package com.zjxjwxk.leetcode._0104_Maximum_Depth_of_Binary_Tree;
 
-import javafx.util.Pair;
-
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -12,23 +10,33 @@ import java.util.Queue;
 public class Solution2 {
 
     public int maxDepth(TreeNode root) {
-        Queue<Pair<TreeNode, Integer>> stack = new LinkedList<>();
+        Queue<Pair> stack = new LinkedList<>();
         if (root != null) {
-            stack.add(new Pair<>(root, 1));
+            stack.add(new Pair(root, 1));
         }
 
         int depth = 0;
         while (!stack.isEmpty()) {
-            Pair<TreeNode, Integer> current = stack.poll();
-            root = current.getKey();
-            int currentDepth = current.getValue();
+            Pair current = stack.poll();
+            root = current.root;
+            int currentDepth = current.depth;
             if (root != null) {
                 depth = Math.max(depth, currentDepth);
-                stack.add(new Pair<>(root.left, currentDepth + 1));
-                stack.add(new Pair<>(root.right, currentDepth + 1));
+                stack.add(new Pair(root.left, currentDepth + 1));
+                stack.add(new Pair(root.right, currentDepth + 1));
             }
         }
         return depth;
+    }
+
+    static class Pair {
+        public TreeNode root;
+        public Integer depth;
+
+        public Pair(TreeNode root, Integer depth) {
+            this.root = root;
+            this.depth = depth;
+        }
     }
 
     public static void main(String[] args) {
