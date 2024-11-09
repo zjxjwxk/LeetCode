372f3e47f242ded9f2966460126a50c6d71eeaa1
zjxjwxk <zjxjwxk@gmail.com>
2024-11-09 23:01:08 +0800

    Use AssertJ instead of AnyOrderList

--- src/com/zjxjwxk/leetcode/_0093_Restore_IP_Addresses/SolutionTest.java
+++ src/com/zjxjwxk/leetcode/_0093_Restore_IP_Addresses/SolutionTest.java
@@ -1,12 +1,12 @@
 package com.zjxjwxk.leetcode._0093_Restore_IP_Addresses;
 
-import com.zjxjwxk.leetcode.util.AnyOrderList;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
 
@@ -16,20 +16,20 @@ class SolutionTest {
     void restoreIpAddresses() {
         String s = "25525511135";
         List<String> ans = Arrays.asList("255.255.11.135", "255.255.111.35");
-        assertEquals(new AnyOrderList(ans), new AnyOrderList(solution.restoreIpAddresses(s)));
+        assertThat(solution.restoreIpAddresses(s)).containsExactlyInAnyOrderElementsOf(ans);
     }
 
     @Test
     void restoreIpAddresses2() {
         String s = "0000";
-        List<String> ans = Arrays.asList("0.0.0.0");
-        assertEquals(new AnyOrderList(ans), new AnyOrderList(solution.restoreIpAddresses(s)));
+        List<String> ans = Collections.singletonList("0.0.0.0");
+        assertThat(solution.restoreIpAddresses(s)).containsExactlyInAnyOrderElementsOf(ans);
     }
 
     @Test
     void restoreIpAddresses3() {
         String s = "101023";
         List<String> ans = Arrays.asList("1.0.10.23", "1.0.102.3", "10.1.0.23", "10.10.2.3", "101.0.2.3");
-        assertEquals(new AnyOrderList(ans), new AnyOrderList(solution.restoreIpAddresses(s)));
+        assertThat(solution.restoreIpAddresses(s)).containsExactlyInAnyOrderElementsOf(ans);
     }
 }
