1f89f236639cf1fc042836a29cb1c9a471da70cd
zjxjwxk <zjxjwxk@gmail.com>
2025-06-11 20:56:31 +0800

    Add 240 UT

--- src/com/zjxjwxk/leetcode/_0240_Search_a_2D_Matrix_II/Solution.java
+++ src/com/zjxjwxk/leetcode/_0240_Search_a_2D_Matrix_II/Solution.java
@@ -5,7 +5,7 @@ package com.zjxjwxk.leetcode._0240_Search_a_2D_Matrix_II;
  * @date 2020/7/26 11:32 下午
  */
 public class Solution {
-    public static boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
@@ -20,16 +20,5 @@ public class Solution {
             }
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-        int[][] arr = {
-                {1,   4,  7, 11, 15},
-                {2,   5,  8, 12, 19},
-                {3,   6,  9, 16, 22},
-                {10, 13, 14, 17, 24},
-                {18, 21, 23, 26, 30}
-        };
-        System.out.println(searchMatrix(arr, 20));
     }
 }
---
+++ src/com/zjxjwxk/leetcode/_0240_Search_a_2D_Matrix_II/SolutionTest.java
@@ -0,0 +1,24 @@
+package com.zjxjwxk.leetcode._0240_Search_a_2D_Matrix_II;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    void searchMatrix() {
+        int[][] matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+        int target = 5;
+        assertTrue(solution.searchMatrix(matrix, target));
+    }
+
+    @Test
+    void searchMatrix2() {
+        int[][] matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+        int target = 20;
+        assertFalse(solution.searchMatrix(matrix, target));
+    }
+}
