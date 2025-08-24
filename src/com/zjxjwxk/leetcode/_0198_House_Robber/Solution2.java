e9723c18add7809654fa5593ed908e31dc6deda8
zjxjwxk <zjxjwxk@gmail.com>
2025-08-24 13:30:25 +0800

    Improve 198 Solution2

--- src/com/zjxjwxk/leetcode/_0198_House_Robber/Solution2.java
+++ src/com/zjxjwxk/leetcode/_0198_House_Robber/Solution2.java
@@ -8,15 +8,12 @@ package com.zjxjwxk.leetcode._0198_House_Robber;
 public class Solution2 {
 
     public int rob(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
+        int dp1 = 0, dp2 = 0;
+        for (int num : nums) {
+            int max = Math.max(dp1 + num, dp2);
+            dp1 = dp2;
+            dp2 = max;
         }
-        int first = nums[0], second = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < nums.length; ++i) {
-            int temp = second;
-            second = Math.max(first + nums[i], second);
-            first = temp;
-        }
-        return second;
+        return dp2;
     }
 }
