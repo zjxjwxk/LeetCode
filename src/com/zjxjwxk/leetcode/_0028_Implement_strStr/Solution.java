30e129ca6ce65633d82b16c775f2cd5a479f14e8
zjxjwxk <zjxjwxk@gmail.com>
2025-10-11 12:39:52 +0800

    Improve 28

--- src/com/zjxjwxk/leetcode/_0028_Implement_strStr/Solution.java
+++ src/com/zjxjwxk/leetcode/_0028_Implement_strStr/Solution.java
@@ -6,25 +6,15 @@ package com.zjxjwxk.leetcode._0028_Implement_strStr;
 public class Solution {
 
     public int strStr(String haystack, String needle) {
-        int len1 = haystack.length(), len2 = needle.length();
-        char[] chArr1 = haystack.toCharArray(), chArr2 = needle.toCharArray();
-        if (len2 == 0) {
-            return 0;
-        }
-        if (len2 > len1) {
-            return -1;
-        }
-        for (int i = 0; i < len1; ++i) {
-            if (i + len2 > len1) {
-                return -1;
-            }
-            int j;
-            for (j = 0; j < len2; ++j) {
-                if (chArr2[j] != chArr1[i + j]) {
+        for (int i = 0; i <= haystack.length() - needle.length(); ++i) {
+            boolean flag = true;
+            for (int j = 0; j < needle.length(); ++j) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
                     break;
                 }
             }
-            if (j == len2) {
+            if (flag) {
                 return i;
             }
         }
