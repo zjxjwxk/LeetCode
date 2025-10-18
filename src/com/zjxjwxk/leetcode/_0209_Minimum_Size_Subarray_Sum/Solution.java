f3fb3a7f22328976e9d6112c90541631768aa0ea
zjxjwxk <zjxjwxk@gmail.com>
2025-10-18 11:35:29 +0800

    Improve 209

--- src/com/zjxjwxk/leetcode/_0209_Minimum_Size_Subarray_Sum/Solution.java
+++ src/com/zjxjwxk/leetcode/_0209_Minimum_Size_Subarray_Sum/Solution.java
@@ -9,19 +9,14 @@ package com.zjxjwxk.leetcode._0209_Minimum_Size_Subarray_Sum;
 public class Solution {
 
     public int minSubArrayLen(int target, int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int i = 0, j = 0, len = nums.length, sum = 0, minLen = Integer.MAX_VALUE;
+        int len = nums.length, i = 0, j = 0, sum = 0, ans = Integer.MAX_VALUE;
         while (j < len) {
-            while (j < len && sum < target) {
-                sum += nums[j++];
-            }
-            while (i < j && sum >= target) {
-                minLen = Math.min(minLen, j - i);
+            sum += nums[j++];
+            while (sum >= target) {
+                ans = Math.min(ans, j - i);
                 sum -= nums[i++];
             }
         }
-        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+        return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 }
