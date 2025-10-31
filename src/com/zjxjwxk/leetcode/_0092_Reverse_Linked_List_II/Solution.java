e62c99534307204a3160f5f783c8d964aa5dd448
zjxjwxk <zjxjwxk@gmail.com>
2025-10-31 13:43:59 +0800

    Improve 92

--- src/com/zjxjwxk/leetcode/_0092_Reverse_Linked_List_II/Solution.java
+++ src/com/zjxjwxk/leetcode/_0092_Reverse_Linked_List_II/Solution.java
@@ -3,31 +3,28 @@ package com.zjxjwxk.leetcode._0092_Reverse_Linked_List_II;
 import com.zjxjwxk.leetcode.util.ListNode;
 
 /**
+ * 头插法
+ *
  * @author Xinkang Wu
  * @date 2021/3/18 11:55
  */
 public class Solution {
 
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode dummyHead = new ListNode();
-        dummyHead.next = head;
-        ListNode pre = dummyHead;
+        ListNode dummyHead = new ListNode(0, head), p = dummyHead;
         for (int i = 0; i < left - 1; ++i) {
-            pre = pre.next;
+            p = p.next;
         }
-        ListNode behind = pre;
-        for (int i = 0; i < right - left + 2; ++i) {
-            behind = behind.next;
-        }
-        ListNode newHead = pre.next, newTail = newHead, p = newHead.next, temp;
-        while (p != behind) {
-            temp = p.next;
+        ListNode prev = p, newTail = p.next, newHead = null, next = null;
+        p = p.next;
+        for (int i = left; i <= right; ++i) {
+            next = p.next;
             p.next = newHead;
             newHead = p;
-            p = temp;
+            p = next;
         }
-        pre.next = newHead;
-        newTail.next = behind;
+        prev.next = newHead;
+        newTail.next = next;
         return dummyHead.next;
     }
 }
