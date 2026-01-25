5e19e287a272def4d6d7f04942f52dca50a35463
zjxjwxk <zjxjwxk@gmail.com>
2026-01-25 15:44:14 +0800

    Add 452 Solution2

--- src/com/zjxjwxk/leetcode/_0452_Minimum_Number_of_Arrows_to_Burst_Balloons/Solution2.java
+++ src/com/zjxjwxk/leetcode/_0452_Minimum_Number_of_Arrows_to_Burst_Balloons/Solution2.java
@@ -4,23 +4,25 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 /**
- * 按右边界排序+贪心
+ * 按左边界排序+贪心
  *
  * @author Xinkang Wu
  * @date 2020/11/23 11:19
  */
-public class Solution {
+public class Solution2 {
 
     public int findMinArrowShots(int[][] points) {
         if (points.length == 0) {
             return 0;
         }
-        Arrays.sort(points, Comparator.comparingInt(o -> o[1]));
+        Arrays.sort(points, Comparator.comparingInt(o -> o[0]));
         int ans = 1, endIndex = points[0][1];
         for (int[] point : points) {
             if (point[0] > endIndex) {
                 endIndex = point[1];
                 ++ans;
+            } else {
+                endIndex = Math.min(endIndex, point[1]);
             }
         }
         return ans;
--- src/com/zjxjwxk/leetcode/_0452_Minimum_Number_of_Arrows_to_Burst_Balloons/Solution2Test.java
+++ src/com/zjxjwxk/leetcode/_0452_Minimum_Number_of_Arrows_to_Burst_Balloons/Solution2Test.java
@@ -2,58 +2,58 @@ package com.zjxjwxk.leetcode._0452_Minimum_Number_of_Arrows_to_Burst_Balloons;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SolutionTest {
+class Solution2Test {
 
-    private final Solution solution = new Solution();
+    private final Solution2 solution2 = new Solution2();
 
     @Test
     void findMinArrowShots1() {
         int[][] points = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
         int ans = 2;
-        assertEquals(ans, solution.findMinArrowShots(points));
+        assertEquals(ans, solution2.findMinArrowShots(points));
     }
 
     @Test
     void findMinArrowShots2() {
         int[][] points = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
         int ans = 4;
-        assertEquals(ans, solution.findMinArrowShots(points));
+        assertEquals(ans, solution2.findMinArrowShots(points));
     }
 
     @Test
     void findMinArrowShots3() {
         int[][] points = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
         int ans = 2;
-        assertEquals(ans, solution.findMinArrowShots(points));
+        assertEquals(ans, solution2.findMinArrowShots(points));
     }
 
     @Test
     void findMinArrowShots4() {
         int[][] points = {{1, 2}};
         int ans = 1;
-        assertEquals(ans, solution.findMinArrowShots(points));
+        assertEquals(ans, solution2.findMinArrowShots(points));
     }
 
     @Test
     void findMinArrowShots5() {
         int[][] points = {{2, 3}, {2, 3}};
         int ans = 1;
-        assertEquals(ans, solution.findMinArrowShots(points));
+        assertEquals(ans, solution2.findMinArrowShots(points));
     }
 
     @Test
     void findMinArrowShots6() {
         int[][] points = {{-2147483646, -2147483645}, {2147483646, 2147483647}};
         int ans = 2;
-        assertEquals(ans, solution.findMinArrowShots(points));
+        assertEquals(ans, solution2.findMinArrowShots(points));
     }
 
     @Test
     void findMinArrowShots7() {
         int[][] points = {{3, 9}, {7, 12}, {3, 8}, {6, 8}, {9, 10}, {2, 9}, {0, 9}, {3, 9}, {0, 6}, {2, 8}};
         int ans = 2;
-        assertEquals(ans, solution.findMinArrowShots(points));
+        assertEquals(ans, solution2.findMinArrowShots(points));
     }
 }
