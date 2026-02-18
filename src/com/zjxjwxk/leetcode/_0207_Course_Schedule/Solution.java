9634bc234d0c985d6893def1d6f84aed3af3c905
zjxjwxk <zjxjwxk@gmail.com>
2026-02-18 20:47:04 +0800

    Improve 207 Solution

--- src/com/zjxjwxk/leetcode/_0207_Course_Schedule/Solution.java
+++ src/com/zjxjwxk/leetcode/_0207_Course_Schedule/Solution.java
@@ -3,45 +3,45 @@ package com.zjxjwxk.leetcode._0207_Course_Schedule;
 import java.util.*;
 
 /**
- * 深度优先搜索
+ * 深度优先搜索（状态机）
  * @author Xinkang Wu
  * @date 2021/12/14 12:06
  */
 public class Solution {
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> preCourseMap = new HashMap<>();
-        for (int[] prerequisite : prerequisites) {
-            int course = prerequisite[0];
-            List<Integer> preCourseList = preCourseMap.getOrDefault(course, new ArrayList<>());
-            preCourseList.add(prerequisite[1]);
-            preCourseMap.put(course, preCourseList);
+        List<List<Integer>> graph = new ArrayList<>(numCourses);
+        int[] states = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
         }
-        int[] status = new int[numCourses];
-        for (int i = 0; i < numCourses; ++i) {
-            if (!dfs(preCourseMap, status, i)) {
+        for (int[] prerequisite : prerequisites) {
+            graph.get(prerequisite[1]).add(prerequisite[0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(graph, states, i)) {
                 return false;
             }
         }
         return true;
     }
 
-    private boolean dfs(Map<Integer, List<Integer>> preCourseMap, int[] status, int course) {
-        if (status[course] == 2) {
-            return true;
-        }
-        List<Integer> preCourseList = preCourseMap.get(course);
-        if (preCourseList == null) {
-            status[course] = 2;
-            return true;
-        }
-        status[course] = 1;
-        for (int preCourse : preCourseList) {
-            if (status[preCourse] == 1 || !dfs(preCourseMap, status, preCourse)) {
+    private boolean dfs(List<List<Integer>> graph, int[] states, int course) {
+        int state = states[course];
+        switch(state) {
+            case 1:
                 return false;
-            }
+            case 2:
+                return true;
+            case 0:
+                states[course] = 1;
+                for (Integer next : graph.get(course)) {
+                    if (!dfs(graph, states, next)) {
+                        return false;
+                    }
+                }
         }
-        status[course] = 2;
+        states[course] = 2;
         return true;
     }
 }
