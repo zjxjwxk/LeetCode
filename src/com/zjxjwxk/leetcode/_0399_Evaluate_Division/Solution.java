26f4fdec88b4b95559878346478b71c1c155efe9
zjxjwxk <zjxjwxk@gmail.com>
2026-02-18 17:47:26 +0800

    Improve 399

--- src/com/zjxjwxk/leetcode/_0399_Evaluate_Division/Solution.java
+++ src/com/zjxjwxk/leetcode/_0399_Evaluate_Division/Solution.java
@@ -12,85 +12,79 @@ import java.util.Map;
 public class Solution {
 
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        int equationsLen = equations.size(), queriesLen = queries.size(), index = 0;
-        Map<String, Integer> indexMap = new HashMap<>(equationsLen << 1);
-        int[] parent = new int[equationsLen << 1];
-        double[] weight = new double[equationsLen << 1];
-        for (int i = 0; i < equationsLen; ++i) {
+        int equationsSize = equations.size();
+        UnionFind unionFind = new UnionFind(equationsSize << 1);
+        Map<String, Integer> strIndexMap = new HashMap<>();
+        int id = 0;
+        for (int i = 0; i < equationsSize; i++) {
             List<String> equation = equations.get(i);
-            String str1 = equation.get(0);
-            String str2 = equation.get(1);
-            Integer index1 = indexMap.get(str1);
-            Integer index2 = indexMap.get(str2);
-            if (index1 == null) {
-                index1 = index++;
-                indexMap.put(str1, index1);
+            String var1 = equation.get(0);
+            String var2 = equation.get(1);
+            if (!strIndexMap.containsKey(var1)) {
+                strIndexMap.put(var1, id++);
             }
-            if (index2 == null) {
-                index2 = index++;
-                indexMap.put(str2, index2);
+            if (!strIndexMap.containsKey(var2)) {
+                strIndexMap.put(var2, id++);
             }
-            weight[index1] = values[i];
+            unionFind.union(strIndexMap.get(var1), strIndexMap.get(var2), values[i]);
         }
-        for (int i = 0; i < index; ++i) {
-            parent[i] = i;
-            if (weight[i] == 0) {
-                weight[i] = 1;
-            }
-        }
-        for (int i = 0; i < equationsLen; ++i) {
-            List<String> equation = equations.get(i);
-            int index1 = indexMap.get(equation.get(0));
-            int index2 = indexMap.get(equation.get(1));
-            union(parent, weight, index1, index2, values[i]);
-        }
-        double[] ans = new double[queriesLen];
-        for (int i = 0; i < queriesLen; ++i) {
+
+        int queriesSize = queries.size();
+        double[] ans = new double[queriesSize];
+        for (int i = 0; i < queriesSize; i++) {
             List<String> query = queries.get(i);
-            Integer index1 = indexMap.get(query.get(0));
-            Integer index2 = indexMap.get(query.get(1));
-            if (index1 == null || index2 == null) {
-                ans[i] = -1;
+            Integer id1 = strIndexMap.get(query.get(0));
+            Integer id2 = strIndexMap.get(query.get(1));
+            if (id1 == null || id2 == null) {
+                ans[i] = -1d;
             } else {
-                int parent1 = (int) findParent(parent, weight, index1)[0];
-                int parent2 = (int) findParent(parent, weight, index2)[0];
-                if (parent1 != parent2) {
-                    ans[i] = -1;
-                } else {
-                    ans[i] = weight[index1] / weight[index2];
-                }
+                ans[i] = unionFind.calculate(id1, id2);
             }
         }
         return ans;
     }
 
-    private void union(int[] parent, double[] weight, int index1, int index2, double value) {
-        double[] ret1 = findParent(parent, weight, index1);
-        double[] ret2 = findParent(parent, weight, index2);
-        if (ret1[0] != ret2[0]) {
-            parent[(int) ret1[0]] = (int) ret2[0];
-            weight[(int) ret1[0]] = value / ret1[1] * ret2[1];
-        }
-    }
+    private class UnionFind {
 
-    private double[] findParent(int[] parent, double[] weight, int index) {
-        double[] ret = new double[2];
-        int root = index;
-        double weightSum = 1;
-        while (root != parent[root]) {
-            weightSum *= weight[root];
-            root = parent[root];
+        private int[] parent;
+        private double[] weight;
+
+        public UnionFind(int n) {
+            this.parent = new int[n];
+            this.weight = new double[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                weight[i] = 1d;
+            }
         }
-        ret[1] = weightSum;
-        while (index != parent[index]) {
-            double w = weight[index];
-            weight[index] = weightSum;
-            weightSum /= w;
-            int tempParent = parent[index];
-            parent[index] = root;
-            index = tempParent;
+
+        public void union(int x, int y, double value) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) {
+                return;
+            }
+            parent[rootX] = rootY;
+            weight[rootX] = value * weight[y] / weight[x];
         }
-        ret[0] = index;
-        return ret;
+
+        public int find(int x) {
+            int cur = x;
+            while (parent[cur] != cur) {
+                cur = parent[cur];
+                weight[x] *= weight[cur];
+            }
+            parent[x] = cur;
+            return cur;
+        }
+
+        public double calculate(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                return -1d;
+            }
+            return weight[x] / weight[y];
+        }
     }
 }
