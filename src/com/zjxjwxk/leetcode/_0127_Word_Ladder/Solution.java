bf12b37a463d81f60cfc3b1bd960c6f2845ff92a
zjxjwxk <zjxjwxk@gmail.com>
2026-02-20 13:55:45 +0800

    Improve 127

--- src/com/zjxjwxk/leetcode/_0127_Word_Ladder/Solution.java
+++ src/com/zjxjwxk/leetcode/_0127_Word_Ladder/Solution.java
@@ -3,45 +3,40 @@ package com.zjxjwxk.leetcode._0127_Word_Ladder;
 import java.util.*;
 
 /**
+ * BFS
+ *
  * @author Xinkang Wu
  * @date 2020/11/5 7:34 下午
  */
 public class Solution {
 
-    private final Map<String, Integer> map = new HashMap<>();
-    private final List<List<Integer>> graph = new ArrayList<>();
-    private boolean[] vis;
-    private int index = 0;
+    private final Map<String, List<String>> graphMap = new HashMap<>();
 
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         for (String word : wordList) {
-            addEdge(word);
+            addWord(word);
         }
-        addEdge(beginWord);
-        if (!map.containsKey(endWord)) {
+        addWord(beginWord);
+        if (!graphMap.containsKey(endWord)) {
             return 0;
         }
-        int beginIndex = map.get(beginWord), endIndex = map.get(endWord);
-        vis = new boolean[graph.size()];
-        return bfs(beginIndex, endIndex);
-    }
-
-    private int bfs(int beginIndex, int endIndex) {
-        Deque<Integer> queue = new LinkedList<>();
-        queue.offer(beginIndex);
-        int level = 0;
+        Queue<String> queue = new LinkedList<>();
+        Set<String> visitedSet = new HashSet<>();
+        queue.offer(beginWord);
+        visitedSet.add(beginWord);
+        int count = 0;
         while (!queue.isEmpty()) {
+            ++count;
             int size = queue.size();
-            ++level;
             for (int i = 0; i < size; ++i) {
-                Integer index = queue.poll();
-                if (index == endIndex) {
-                    return (level >> 1) + 1;
-                }
-                for (Integer next : graph.get(index)) {
-                    if (!vis[next]) {
+                String word = queue.poll();
+                for (String next : graphMap.get(word)) {
+                    if (next.equals(endWord)) {
+                        return (count >> 1) + 1;
+                    }
+                    if (!visitedSet.contains(next)) {
                         queue.offer(next);
-                        vis[next] = true;
+                        visitedSet.add(next);
                     }
                 }
             }
@@ -49,27 +44,27 @@ public class Solution {
         return 0;
     }
 
-    private void addEdge(String word) {
-        addNode(word);
+    private void addWord(String word) {
+        if (graphMap.containsKey(word)) {
+            return;
+        }
         char[] chArr = word.toCharArray();
-        int len = chArr.length, index1 = map.get(word), index2;
-        char temp;
-        for (int i = 0; i < len; ++i) {
-            temp = chArr[i];
+        for (int i = 0; i < chArr.length; ++i) {
+            char ch = chArr[i];
             chArr[i] = '*';
-            String midWord = new String(chArr);
-            addNode(midWord);
-            index2 = map.get(midWord);
-            graph.get(index1).add(index2);
-            graph.get(index2).add(index1);
-            chArr[i] = temp;
+            String patternWord = new String(chArr);
+            addEdge(word, patternWord);
+            addEdge(patternWord, word);
+            chArr[i] = ch;
         }
     }
 
-    private void addNode(String word) {
-        if (!map.containsKey(word)) {
-            map.put(word, index++);
-            graph.add(new ArrayList<>());
+    private void addEdge(String word1, String word2) {
+        List<String> edgeList = graphMap.get(word1);
+        if (edgeList == null) {
+            edgeList = new ArrayList<>();
         }
+        edgeList.add(word2);
+        graphMap.put(word1, edgeList);
     }
 }
