608a48b2206edcd79b868c1c8ab56c2da3e8905c
zjxjwxk <zjxjwxk@gmail.com>
2026-03-01 00:06:23 +0800

    Improve 4 Solution3

--- src/com/zjxjwxk/leetcode/_0004_Median_of_Two_Sorted_Arrays/Solution3.java
+++ src/com/zjxjwxk/leetcode/_0004_Median_of_Two_Sorted_Arrays/Solution3.java
@@ -9,39 +9,35 @@ package com.zjxjwxk.leetcode._0004_Median_of_Two_Sorted_Arrays;
 public class Solution3 {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int len1 = nums1.length, len2 = nums2.length, totalLen = len1 + len2, k1 = (totalLen) >> 1, k2 = k1 + 1;
-        if ((totalLen & 1) == 0) {
-            int mid1 = getKthNum(nums1, nums2, k1);
-            int mid2 = getKthNum(nums1, nums2, k2);
-            return (mid1 + mid2) / 2.0;
+        int len1 = nums1.length, len2 = nums2.length;
+        if (((len1 + len2) & 1) == 0) {
+            int k = (len1 + len2) >> 1;
+            return (getKthNum(nums1, nums2, k) + getKthNum(nums1, nums2, k + 1)) / 2.0d;
         } else {
-            return getKthNum(nums1, nums2, k2);
+            int k = (len1 + len2 + 1) >> 1;
+            return getKthNum(nums1, nums2, k);
         }
     }
 
     private int getKthNum(int[] nums1, int[] nums2, int k) {
-        int left1 = 0, right1 = nums1.length - 1;
-        int left2 = 0, right2 = nums2.length - 1;
-        while (true) {
-            if (left1 > right1) {
-                return nums2[left2 + k - 1];
-            }
-            if (left2 > right2) {
-                return nums1[left1 + k - 1];
-            }
-            if (k == 1) {
-                return Math.min(nums1[left1], nums2[left2]);
-            }
-            int offset = (k >> 1) - 1;
-            int index1 = Math.min(right1, left1 + offset);
-            int index2 = Math.min(right2, left2 + offset);
-            if (nums1[index1] <= nums2[index2]) {
-                k = k - (index1 - left1 + 1);
-                left1 = index1 + 1;
+        int index1 = 0, index2 = 0, len1 = nums1.length, len2 = nums2.length;
+        while (k > 1 && index1 < len1 && index2 < len2) {
+            int mid1 = Math.min(index1 + (k >> 1) - 1, len1 - 1);
+            int mid2 = Math.min(index2 + (k >> 1) - 1, len2 - 1);
+            if (nums1[mid1] <= nums2[mid2]) {
+                k -= mid1 - index1 + 1;
+                index1 = mid1 + 1;
             } else {
-                k = k - (index2 - left2 + 1);
-                left2 = index2 + 1;
+                k -= mid2 - index2 + 1;
+                index2 = mid2 + 1;
             }
         }
+        if (index1 == len1) {
+            return nums2[index2 + k - 1];
+        }
+        if (index2 == len2) {
+            return nums1[index1 + k - 1];
+        }
+        return Math.min(nums1[index1], nums2[index2]);
     }
 }
