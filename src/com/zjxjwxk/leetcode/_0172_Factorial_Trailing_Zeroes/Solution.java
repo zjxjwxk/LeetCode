eea3920b34dc77e561b7094e3fa326b071d72353
zjxjwxk <zjxjwxk@gmail.com>
2026-03-07 14:04:08 +0800

    Correct 172 comment

--- src/com/zjxjwxk/leetcode/_0172_Factorial_Trailing_Zeroes/Solution.java
+++ src/com/zjxjwxk/leetcode/_0172_Factorial_Trailing_Zeroes/Solution.java
@@ -3,7 +3,7 @@ package com.zjxjwxk.leetcode._0172_Factorial_Trailing_Zeroes;
 /**
  * 数学
  * 时间复杂度：O(logn)
- * ∑[n/(5^k)], k属于[1, +∞]
+ * ∑[n/(5^k)], k属于[1, +∞)
  *
  * @author Xinkang Wu
  * @date 2026/3/7 13:54
