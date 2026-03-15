cf75b703164fcfd8b713fc2b0e7883bb09cd0dd4
zjxjwxk <zjxjwxk@gmail.com>
2026-03-15 20:41:19 +0800

    Improve 123 Solution2

--- src/com/zjxjwxk/leetcode/_0123_Best_Time_to_Buy_and_Sell_Stock_III/Solution2.java
+++ src/com/zjxjwxk/leetcode/_0123_Best_Time_to_Buy_and_Sell_Stock_III/Solution2.java
@@ -9,25 +9,13 @@ public class Solution2 {
 
     public int maxProfit(int[] prices) {
         int len = prices.length;
-        int[][][] dp = new int[len][3][2];
-        dp[0][0][0] = 0;
-        dp[0][0][1] = -prices[0];
-        dp[0][1][0] = 0;
-        dp[0][1][1] = -prices[0];
-        dp[0][2][0] = 0;
+        int buy1 = -prices[0], sell1 = 0, buy2 = -prices[0], sell2 = 0;
         for (int i = 1; i < len; ++i) {
-            dp[i][0][0] = dp[i - 1][0][0];
-            dp[i][0][1] = Math.max(dp[i - 1][0][1], dp[i - 1][0][0] - prices[i]);
-            dp[i][1][0] = Math.max(dp[i - 1][1][0], dp[i - 1][0][1] + prices[i]);
-            dp[i][1][1] = Math.max(dp[i - 1][1][1], dp[i - 1][1][0] - prices[i]);
-            dp[i][2][0] = Math.max(dp[i - 1][2][0], dp[i - 1][1][1] + prices[i]);
+            buy1 = Math.max(buy1, -prices[i]);
+            sell1 = Math.max(sell1, buy1 + prices[i]);
+            buy2 = Math.max(buy2, sell1 - prices[i]);
+            sell2 = Math.max(sell2, buy2 + prices[i]);
         }
-        return Math.max(
-                Math.max(
-                        dp[len - 1][0][0],
-                        dp[len - 1][1][0]
-                ),
-                dp[len - 1][2][0]
-        );
+        return sell2;
     }
 }
